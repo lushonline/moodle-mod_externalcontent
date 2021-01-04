0fb255c3082ae6a2d0ad65f99d2eda052eea6d6c
lushonline <56685070+lushonline@users.noreply.github.com>
2021-01-04 09:54:20 +0000

    Version bump

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020122100;
+$plugin->version = 2021010400;
 $plugin->requires = 2018051700; // Requires this Moodle version v3.5 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'mod_externalcontent';
 $plugin->maturity = MATURITY_BETA;
