e7c4684208fe3eecc5bd94834acdadef54df99ef
lushonline <56685070+lushonline@users.noreply.github.com>
2021-01-04 09:52:46 +0000

    Update aboutcontroller.php
    Remove config.php require, as unused

--- lrs/aboutcontroller.php
+++ lrs/aboutcontroller.php
@@ -23,7 +23,6 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-require_once('../../../config.php');
 require('./vendor/autoload.php');
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
