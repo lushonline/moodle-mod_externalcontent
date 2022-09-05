8b2efe2d6400c489c9cd21d36b18daa888442545
lushonline <56685070+lushonline@users.noreply.github.com>
2022-09-05 12:53:27 +0100

    Add a class for import data

---
+++ classes/importrecord.php
@@ -0,0 +1,246 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Import record for mod_externalcontent
+ *
+ * @package     mod_externalcontent
+ * @copyright   2019-2022 LushOnline
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_externalcontent;
+
+/**
+ * Import record for mod_externalcontent
+ *
+ * @package     mod_externalcontent
+ * @copyright   2019-2022 LushOnline
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class importrecord {
+
+    /** @var stdClass The course import data */
+    protected $courseimport;
+
+    /** @var stdClass The module import data */
+    protected $moduleimport;
+
+    /**
+     * Return an empty course import record.
+     *
+     * @return stdClass
+     */
+    private function get_default_courseimport(): \stdClass {
+        $course = new \stdClass();
+        $course->idnumber = null;
+        $course->shortname = null;
+        $course->fullname = null;
+        $course->summary = null;
+        $course->summaryformat = 1; // FORMAT_HTML.
+        $course->tags = array();
+        $course->visible = null;
+        $course->thumbnail = null;
+
+        $course->format = "singleactivity";
+        $course->numsections = 0;
+        $course->newsitems = 0;
+        $course->showgrades = 0;
+        $course->showreports = 0;
+        $course->startdate = time();
+        $course->activitytype = "externalcontent";
+        $course->enablecompletion = 1;
+
+        $course->category = null;
+
+        $course->readonly = array(
+            'summaryformat',
+            'format',
+            'numsections',
+            'newsitems',
+            'startdate',
+            'activitytype',
+            'enablecompletion'
+        );
+
+        return $course;
+    }
+
+    /**
+     * Return an empty external content import record.
+     *
+     * @return stdClass
+     */
+    private function get_default_moduleimport(): \stdClass {
+        global $CFG;
+
+        $defaults = get_config('externalcontent');
+
+        $module = new \stdClass();
+        $module->name = null;
+        $module->intro = null;
+        $module->introformat = 1; // FORMAT_HTML.
+        $module->content = null;
+        $module->contentformat = 1; // FORMAT_HTML.
+        $module->markcompleteexternally = 0;
+        $module->completion = 2;
+        $module->completionview = 1;
+
+        $module->printintro = $defaults->printintro;
+        $module->printlastmodified = $defaults->printintro;
+
+        $displayoptions = array();
+        $displayoptions['printintro']   = $defaults->printintro;
+        $displayoptions['printlastmodified'] = $defaults->printlastmodified;
+        $module->displayoptions = serialize($displayoptions);
+
+        $module->readonly = array(
+            'introformat',
+            'contentformat',
+            'completion',
+            'completionview',
+            'displayoptions',
+            'printintro',
+            'printlastmodified'
+        );
+
+        return $module;
+    }
+
+    /**
+     * importrecord constructor
+     *
+     * @param object|null $courseimport - object with one or more properties available for courseimport or null.
+     * @param object|null $moduleimport - object one or more properties available for moduleimport or null.
+     * @return void
+     */
+    public function __construct(?object $courseimport = null, ?object $moduleimport = null) {
+        $this->courseimport = self::get_default_courseimport();
+        $this->moduleimport = self::get_default_moduleimport();
+
+        self::update_courseimport($courseimport);
+        self::update_moduleimport($moduleimport);
+    }
+
+    /**
+     * Validate we have the minimum info for a course.
+     *
+     * @return bool true if validated
+     */
+    private function validate_course() {
+        // As a minimum we need.
+        // course idnumber is not empty.
+        // course shortname is not empty.
+        // course longname is not empty.
+        // course category is an integer.
+
+        $isvalid = true;
+        $isvalid = $isvalid && !empty($this->courseimport->idnumber);
+        $isvalid = $isvalid && !empty($this->courseimport->shortname);
+        $isvalid = $isvalid && !empty($this->courseimport->fullname);
+        $isvalid = $isvalid && is_int($this->courseimport->category);
+        return $isvalid;
+    }
+
+    /**
+     * Validate we have the minimum info for an external content module.
+     *
+     * @return bool true if validated
+     */
+    private function validate_module() {
+        // As a minimum we need.
+        // external name is not empty.
+        // external intro is not empty.
+        // external content is not empty.
+        $isvalid = true;
+        $isvalid = $isvalid && !empty($this->moduleimport->name);
+        $isvalid = $isvalid && !empty($this->moduleimport->intro);
+        $isvalid = $isvalid && !empty($this->moduleimport->content);
+        return $isvalid;
+    }
+
+
+    /**
+     * Validate we have the minimum info to create/update course and externalcontent module
+     *
+     * @return bool true if validated
+     */
+    public function validate() : bool {
+        return self::validate_course() && self::validate_module();
+    }
+
+    /**
+     * Get the course import record.
+     *
+     * @return stdClass|bool - returns the course import record or false if not valid
+     */
+    public function get_courseimport(): \stdClass {
+        return $this->courseimport;
+    }
+
+    /**
+     * Update the course import record, supplied values override current.
+     * Only values not defined as readonly updated.
+     *
+     * @param object|null $updates - the courseimport updates or null.
+     * @return bool
+     */
+    public function update_courseimport(?object $updates): bool {
+        if (is_null($updates)) {
+            return false;
+        }
+
+        // Loop thru all properties of this course import object.
+        foreach ($this->courseimport as $k => $v) {
+            // Check if passed object has property and it isnt readonly.
+            if (isset($updates->$k) && !in_array($k, $this->courseimport->readonly)) {
+                $this->courseimport->$k = $updates->$k;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Get the module import record.
+     *
+     * @return stdClass|bool - returns the module import record or false if not valid
+     */
+    public function get_moduleimport(): \stdClass {
+        return $this->moduleimport;
+    }
+
+    /**
+     * Update the module import record, supplied values override current.
+     * Only values not defined as readonly updated.
+     *
+     * @param object|null $updates - the moduleimport updates or null.
+     * @return bool
+     */
+    public function update_moduleimport(?object $updates): bool {
+        if (is_null($updates)) {
+            return false;
+        }
+
+        // Loop thru all properties of this module import object.
+        foreach ($this->moduleimport as $k => $v) {
+            // Check if passed object has property and it isnt readonly.
+            if (isset($updates->$k) && !in_array($k, $this->moduleimport->readonly)) {
+                $this->moduleimport->$k = $updates->$k;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ tests/importrecord_test.php
@@ -0,0 +1,268 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+namespace mod_externalcontent;
+
+use advanced_testcase;
+use moodle_exception;
+
+/**
+ * Tests for the External Content Import Record.
+ *
+ * @package     mod_externalcontent
+ * @copyright   2019-2022 LushOnline
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @coversDefaultClass \mod_externalcontent\importrecord
+ */
+class importrecord_test extends advanced_testcase {
+
+    /**
+     * Get the courseimport record form importrecord
+     * @covers \mod_externalcontent\importrecord::get_courseimport()
+     */
+    public function test_get_courseimport(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $result = $importrecord->get_courseimport();
+
+        // Check courseimport it is stdClass type.
+        $this->assertInstanceOf(\stdClass::class, $result);
+
+        // Check the null values.
+        $this->assertNull($result->idnumber);
+        $this->assertNull($result->shortname);
+        $this->assertNull($result->fullname);
+        $this->assertNull($result->summary);
+        $this->assertNull($result->visible);
+        $this->assertNull($result->thumbnail);
+        $this->assertNull($result->category);
+
+        // Check the default values are equal.
+        $this->assertEquals($result->format, "singleactivity");
+        $this->assertEquals($result->numsections, 0);
+        $this->assertEquals($result->newsitems, 0);
+        $this->assertEquals($result->showgrades, 0);
+        $this->assertEquals($result->showreports, 0);
+        $this->assertEquals($result->activitytype, "externalcontent");
+        $this->assertEquals($result->enablecompletion, 1);
+        $this->assertEquals($result->summaryformat, 1);
+
+        // Assert start time is less than or equal to time now.
+        $this->assertLessThanOrEqual($result->startdate, time());
+
+        // Confirm tags is array.
+        $this->assertIsArray($result->tags);
+    }
+
+    /**
+     * Get the moduleimport record from importrecord
+     * @covers \mod_externalcontent\importrecord::get_moduleimport()
+     */
+    public function test_get_moduleimport(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $moduleimport = $importrecord->get_moduleimport();
+
+        // Check it is stdClass type.
+        $this->assertInstanceOf(\stdClass::class, $moduleimport);
+
+        // Check the null values.
+        $this->assertNull($moduleimport->name);
+        $this->assertNull($moduleimport->intro);
+        $this->assertNull($moduleimport->content);
+
+        // Check the default values are equal.
+        $this->assertEquals($moduleimport->markcompleteexternally, 0);
+        $this->assertEquals($moduleimport->introformat, 1);
+        $this->assertEquals($moduleimport->contentformat, 1);
+        $this->assertEquals($moduleimport->completion, 2);
+        $this->assertEquals($moduleimport->completionview, 1);
+    }
+
+    /**
+     * The courseimport record updating
+     * @covers \mod_externalcontent\importrecord::update_courseimport()
+     */
+    public function test_update_courseimport(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalcourse = $importrecord->get_courseimport();
+
+        $clonedcourse = clone $originalcourse;
+        $clonedcourse->idnumber = "none empty string ".time();
+
+        $importrecord->update_courseimport($clonedcourse);
+        $result = $importrecord->get_courseimport();
+
+        // Check the values have been updated.
+        $this->assertEquals($result, $clonedcourse);
+        $this->assertEquals($result->idnumber, $clonedcourse->idnumber);
+    }
+
+    /**
+     * The courseimport record ignores values
+     * @covers \mod_externalcontent\importrecord::update_courseimport()
+     */
+    public function test_update_courseimport_ignore(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalcourse = $importrecord->get_courseimport();
+
+        $clonedcourse = clone $originalcourse;
+        $clonedcourse->ignored = "none empty string ".time();
+
+        $importrecord->update_courseimport($clonedcourse);
+        $result = $importrecord->get_courseimport();
+
+        // Check the the values has been ignored.
+        $this->assertEquals($result, $originalcourse);
+        $this->assertObjectNotHasAttribute('ignored', $result);
+    }
+
+    /**
+     * The courseimport record does not update readonly
+     * @covers \mod_externalcontent\importrecord::update_courseimport()
+     */
+    public function test_update_courseimport_readonly(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalcourse = $importrecord->get_courseimport();
+
+        $clonedcourse = clone $originalcourse;
+        $clonedcourse->activitytype = "none empty string ".time();
+        $clonedcourse->summaryformat = "none empty string ".time();
+        $clonedcourse->format = "none empty string ".time();
+        $clonedcourse->numsections = "none empty string ".time();
+        $clonedcourse->newsitems = "none empty string ".time();
+        $clonedcourse->startdate = "none empty string ".time();
+        $clonedcourse->activitytype = "none empty string ".time();
+        $clonedcourse->enablecompletion = "none empty string ".time();
+
+        $importrecord->update_courseimport($clonedcourse);
+        $result = $importrecord->get_courseimport();
+
+        // Check the courseimport was not updated.
+        $this->assertEquals($result, $originalcourse);
+    }
+
+    /**
+     * The moduleimport record updating
+     * @covers \mod_externalcontent\importrecord::update_moduleimport()
+     */
+    public function test_update_moduleimport(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalmodule = $importrecord->get_moduleimport();
+
+        $clonedmodule = clone $originalmodule;
+        $clonedmodule->name = "none empty string ".time();
+
+        $importrecord->update_moduleimport($clonedmodule);
+        $result = $importrecord->get_moduleimport();
+
+        // Check the default values are equal.
+        $this->assertEquals($result, $clonedmodule);
+    }
+
+    /**
+     * The moduleimport record ignores values
+     * @covers \mod_externalcontent\importrecord::update_moduleimport()
+     */
+    public function test_update_moduleimport_ignore(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalmodule = $importrecord->get_moduleimport();
+
+        $clonedmodule = clone $originalmodule;
+        $clonedmodule->ignored = "none empty string ".time();
+
+        $importrecord->update_moduleimport($clonedmodule);
+        $result = $importrecord->get_moduleimport();
+
+        // Check the the values has been ignored.
+        $this->assertEquals($result, $originalmodule);
+        $this->assertObjectNotHasAttribute('ignored', $result);
+    }
+
+    /**
+     * The moduleimport record does not update readonly
+     * @covers \mod_externalcontent\importrecord::update_moduleimport()
+     */
+    public function test_update_moduleimport_readonly(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $originalmodule = $importrecord->get_moduleimport();
+
+        $clonedmodule = clone $originalmodule;
+        $clonedmodule->introformat = "none empty string ".time();
+        $clonedmodule->contentformat = "none empty string ".time();
+        $clonedmodule->completion = "none empty string ".time();
+        $clonedmodule->completionview = "none empty string ".time();
+
+        $importrecord->update_moduleimport($clonedmodule);
+        $result = $importrecord->get_moduleimport();
+
+        // Check the moduleimport was not updated.
+        $this->assertEquals($result, $originalmodule);
+    }
+
+    /**
+     * Test validate fails
+     * @covers \mod_externalcontent\importrecord::validate()
+     */
+    public function test_validate_fail(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+
+        // Check it successed.
+        $this->assertFalse($importrecord->validate());
+    }
+
+    /**
+     * Test validate passes
+     * @covers \mod_externalcontent\importrecord::validate()
+     */
+    public function test_validate_pass(): void {
+        $this->resetAfterTest();
+
+        $importrecord = new importrecord();
+        $courseimport = $importrecord->get_courseimport();
+        $moduleimport = $importrecord->get_moduleimport();
+
+        // Set the minimum values.
+        $courseimport->idnumber = "none empty string";
+        $courseimport->shortname = "none empty string";
+        $courseimport->fullname = "none empty string";
+        $courseimport->category = 0;
+        $importrecord->update_courseimport($courseimport);
+
+        $moduleimport->name = "none empty string";
+        $moduleimport->intro = "none empty string";
+        $moduleimport->content = "none empty string";
+        $importrecord->update_moduleimport($moduleimport);
+
+        // Check it succeeds.
+        $this->assertTrue($importrecord->validate());
+    }
+}
