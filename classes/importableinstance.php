ccf864965e58d7fa3d66e61a720eda4207f260bf
lushonline <56685070+lushonline@users.noreply.github.com>
2022-09-14 08:57:26 +0100

    Fix PHPMD "Avoid unused local variables"

--- classes/importableinstance.php
+++ classes/importableinstance.php
@@ -281,9 +281,8 @@ class importableinstance extends instance {
         if (\core_tag_tag::is_enabled('core', 'course') && count($courseimport->tags) > 0) {
             $existingtags = self::get_course_tags($instance->get_course_id());
 
-            if (!$tagsmatch = (
-                array_diff($existingtags, $courseimport->tags) == [] &&
-                array_diff($courseimport->tags, $existingtags) == [])) {
+            if (array_diff($existingtags, $courseimport->tags) == [] &&
+                array_diff($courseimport->tags, $existingtags) == []) {
                     \core_tag_tag::set_item_tags('core', 'course',
                                                  $instance->get_course_id(),
                                                  $instance->get_context_course(),
@@ -341,7 +340,7 @@ class importableinstance extends instance {
      * @return null|instance
      */
     public static function get_from_importrecord(importrecord $importrecord): ?instance {
-        if ($valid = $importrecord->validate()) {
+        if ($importrecord->validate()) {
             if ($result = self::get_from_cmidnumber($importrecord->get_courseimport()->idnumber)) {
                 // Update.
                 return self::update_from_importrecord($result, $importrecord);
